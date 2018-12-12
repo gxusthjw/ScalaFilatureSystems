9e841052e44015c85fad52679dc03e8b2e51ddff
JiweiHuang <huangjiwei@gxust.edu.cn>
2018-12-12 14:21:56 +0800

    update

--- src/main/scala/cn/edu/gxust/jiweihuang/scala/filature/bave/bave.scala
+++ src/main/scala/cn/edu/gxust/jiweihuang/scala/filature/bave/bave.scala
@@ -1,9 +1,12 @@
 package cn.edu.gxust.jiweihuang.scala.filature.bave
 
 import cn.edu.gxust.jiweihuang.scala.math.function.univariate.TQuadraticVertexLogistic
+import org.hipparchus.analysis.ParametricUnivariateFunction
+
+import math._
 
 trait Bave {
-  val baveLength: Double
+  var baveLength: Double
 
   def baveSize(pos: Double): Double
 }
@@ -15,21 +18,32 @@ trait TQVLBaveModel extends Bave with TQuadraticVertexLogistic {
   val terminalSize: Double = 1.8
   val maxSizePos: Double = 200
 
-  override val quadraticVertexA: Double = _
-  override val quadraticVertexB: Double = _
+  override val quadraticVertexA: Double = (initialSize - terminalSize) * (1 + exp(logisticK * maxSizePos)) / pow(modelBaveLength, 2)
+  override val quadraticVertexB: Double = modelBaveLength
   override val quadraticVertexC: Double = 0
   override val logisticM: Double = 1
-  override val logisticK: Double = _
-  override val logisticX0: Double = _
-  override val quadraticVertexLogisticD: Double = _
+  override val logisticK: Double = 4 / (modelBaveLength - maxSizePos)
+  override val logisticX0: Double = maxSizePos
+  override val quadraticVertexLogisticD: Double = terminalSize
 
   var lowerPos: Double = 0
   var upperPos: Double = modelBaveLength
-  override val baveLength: Double = upperPos - lowerPos
+  override var baveLength: Double = upperPos - lowerPos
 
   override def baveSize(pos: Double): Double = {
     value(pos)
   }
+
+}
+
+object TQVLBaveModel {
+
+  class Parametric extends ParametricUnivariateFunction {
+    override def value(x: Double, parameters: Double*): Double = ???
+
+    override def gradient(x: Double, parameters: Double*): Array[Double] = ???
+  }
+
 }
 
 case class QVLBave(override val modelBaveLength: Double = 1200,
@@ -38,23 +52,22 @@ case class QVLBave(override val modelBaveLength: Double = 1200,
                    override val maxSizePos: Double = 200)
   extends TQVLBaveModel
 
-class QVLBaveV1() extends TQVLBaveModel {
-  override val modelBaveLength: Double = _
-  override val initialSize: Double = _
-  override val terminalSize: Double = _
-  override val maxSizePos: Double = _
+class QVLBaveV1(override val modelBaveLength: Double,
+                override val initialSize: Double,
+                val terminalInitialSizeRatio: Double,
+                val maxLengthRatio: Double
+               )
+  extends TQVLBaveModel {
+  override val terminalSize: Double = initialSize * terminalInitialSizeRatio
+  override val maxSizePos: Double = modelBaveLength * maxLengthRatio
 }
 
-class QVLBaveV2() extends TQVLBaveModel {
-  override val modelBaveLength: Double = _
-  override val initialSize: Double = _
-  override val terminalSize: Double = _
-  override val maxSizePos: Double = _
-}
+object QVLBaveV1 {
 
-class QVLBaveV3() extends TQVLBaveModel {
-  override val modelBaveLength: Double = _
-  override val initialSize: Double = _
-  override val terminalSize: Double = _
-  override val maxSizePos: Double = _
+  class Parametric extends ParametricUnivariateFunction {
+    override def value(x: Double, parameters: Double*): Double = ???
+
+    override def gradient(x: Double, parameters: Double*): Array[Double] = ???
+  }
+
 }
